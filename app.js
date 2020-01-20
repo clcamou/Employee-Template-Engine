1ee1ecbb23257844d5c4d87a833307c4a8dd990e
clcamou <clcamou@gmail.com>
2020-01-20 12:31:24 -0700

    fixed structure

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // create variables needed for app
 const inquirer = require("inquirer");
-const Manager = require("./lib/")
+const manager = require("./lib/manager");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
+const employee = require("./lib/employee");
+
+const managerTemplate = require("./templates/")
